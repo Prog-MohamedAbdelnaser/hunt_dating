c845ffb9385b18ff4f1d1f1e37c9e5b4b2588620
Ishasaoji <ishasaoji88@gmail.com>
2019-08-20 16:07:48 +0530

    Updated OTP Verification Screen

--- app/src/main/java/com/recep/hunt/login/OtpVerificationActivity.kt
+++ app/src/main/java/com/recep/hunt/login/OtpVerificationActivity.kt
@@ -1,37 +1,35 @@
 package com.recep.hunt.login
 
-import android.animation.ObjectAnimator
 import android.app.Dialog
-import android.app.PendingIntent.getActivity
-import android.content.DialogInterface
 import android.graphics.drawable.ColorDrawable
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
-import android.view.Window
-import android.view.animation.DecelerateInterpolator
+import android.view.View
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.mukesh.OtpView
+import com.goodiebag.pinview.Pinview
 import com.recep.hunt.R
-import com.recep.hunt.utilis.BaseActivity
-import com.recep.hunt.utilis.Custom_Alert_Dialog
 import com.recep.hunt.utilis.launchActivity
 import kotlinx.android.synthetic.main.activity_otp_verification.*
-import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.find
-import org.jetbrains.anko.textColor
+
 
 class OtpVerificationActivity : AppCompatActivity() {
 
     private var pStatus = 0
+    private lateinit var cl_progressbar : ConstraintLayout
+    private lateinit var cl_resend_otp : ConstraintLayout
+    private lateinit var sendButton : Button
     private val handler = Handler()
-    private lateinit var otpPinView : OtpView
+    private lateinit var otpPinView : Pinview
     private lateinit var progressBar : ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,19 +37,42 @@ class OtpVerificationActivity : AppCompatActivity() {
         init()
     }
     private fun init(){
+
         progressBar = find(R.id.otp_progressBar)
+        cl_progressbar=find(R.id.cl_progress_bar)
+        cl_resend_otp=find(R.id.cl_we_will_send_otp)
         otpPinView = find(R.id.otp_pin_view)
-        setupProgressTimer()
+        sendButton=find(R.id.sendButton)
+       // setupProgressTimer()
+        showResendOption()
 
         resend_otp_btn.setOnClickListener {
             showResendOtpAlert()
         }
 
-        otpPinView.setOtpCompletionListener {
+        sendButton.setOnClickListener{
+            setupProgressTimer()
+
+        }
+
+        otpPinView.setPinViewEventListener{ pinview, fromUser ->
             launchActivity<SocialLoginActivity>()
         }
 
 
+    }
+    //Set Resend color Red
+    private fun setSpannable(){
+        val text1= "Didn't receive the code? "
+        val text2= "Resend"
+        val text3 = text1+text2
+        val spannable = SpannableString(text3)
+        spannable.setSpan(
+            ForegroundColorSpan(resources.getColor(R.color.colorPrimary)),
+            text1.length,
+            text3.length,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        resend_otp_btn.setText(spannable, TextView.BufferType.SPANNABLE)
     }
 
     //Resend OTP Alert Dialog
@@ -69,13 +90,17 @@ class OtpVerificationActivity : AppCompatActivity() {
 
     //Setup ProgressTimer
     private fun setupProgressTimer(){
+        cl_progressbar.visibility = View.VISIBLE
+        cl_resend_otp.visibility=View.GONE
         val res = resources
         val drawable = res.getDrawable(R.drawable.circular_progress_bg)
         progressBar.progressDrawable = drawable
         progressBar.progress = 0
-        progressBar.max = 100
+        progressBar.max = 60
+
+
         Thread(Runnable {
-            while (pStatus < 100) {
+            while (pStatus < 60) {
                 pStatus += 1
 
                 handler.post {
@@ -92,7 +117,18 @@ class OtpVerificationActivity : AppCompatActivity() {
                 }
 
             }
+
+
         }).start()
 
+
+
+
+    }
+
+    private fun showResendOption() {
+
+        cl_progressbar.visibility = View.GONE
+        cl_resend_otp.visibility=View.VISIBLE
     }
 }
