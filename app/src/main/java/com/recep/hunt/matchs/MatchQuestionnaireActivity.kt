141cc33181be2d830a3a398c79f061fbbe838da7
mavyasoni <onlysoni3@gmail.com>
2019-11-24 16:56:18 +0530

    [fixed] matchquestion thread issue resolved

--- app/src/main/java/com/recep/hunt/matchs/MatchQuestionnaireActivity.kt
+++ app/src/main/java/com/recep/hunt/matchs/MatchQuestionnaireActivity.kt
@@ -1,23 +1,19 @@
 package com.recep.hunt.matchs
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.recep.hunt.R
 import kotlinx.android.synthetic.main.activity_match_questionnaire.*
 import org.jetbrains.anko.find
 import java.util.concurrent.TimeUnit
-
-
-
-
-
-
 
 class MatchQuestionnaireActivity : AppCompatActivity() {
 
@@ -25,76 +21,91 @@ class MatchQuestionnaireActivity : AppCompatActivity() {
     private var pStatusVisible = 15
     private lateinit var cl_progressbar: ConstraintLayout
     private lateinit var progressBar: ProgressBar
+    private lateinit var pbMatchTime: ProgressBar
     private val handler = Handler()
-    private var currentTime=0L
+    private var currentTime = 0L
 
-    private lateinit var cl_timer_progrss:ConstraintLayout
-    private lateinit var progressBarTimer:ProgressBar
-    private val timerHandler=Handler()
+    private lateinit var cl_timer_progrss: ConstraintLayout
+    private lateinit var progressBarTimer: ProgressBar
+    private val timerHandler = Handler()
 
-    lateinit var countDownTimer:CountDownTimer
-    private var timerPstatus=360
+    lateinit var countDownTimer: CountDownTimer
+    private var timerPstatus = 360
+
+    private var addTime = 1L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_questionnaire)
         progressBar = find(R.id.otp_progressBar)
+        pbMatchTime = find(R.id.pbTimer)
         cl_progressbar = find(R.id.cl_progress_bar_two)
 
 
         check_code_btn.setOnClickListener {
-            setpTwo.visibility=View.VISIBLE
-            setpOne.visibility=View.GONE
+            setpTwo.visibility = View.VISIBLE
+            setpOne.visibility = View.GONE
             setProgressStart()
         }
 
         btn_yes.setOnClickListener {
-            setpThree.visibility=View.GONE
-            stepFour.visibility=View.VISIBLE
+            setpThree.visibility = View.GONE
+            stepFour.visibility = View.VISIBLE
 
         }
         btn_goto_next_five.setOnClickListener {
-            stepFour.visibility=View.GONE
-            stepFive.visibility=View.VISIBLE
+            stepFour.visibility = View.GONE
+            stepFive.visibility = View.VISIBLE
 
         }
 
         btn.setOnClickListener {
-            stepFive.visibility=View.GONE
-            stepSix.visibility=View.VISIBLE
-            setTimer(6*60*1000)
-
+            stepFive.visibility = View.GONE
+            stepSix.visibility = View.VISIBLE
+            setTimer(6 * 60 * 1000)
 
         }
 
         idSubmit.setOnClickListener {
-            tvStepSix.visibility=View.GONE
-            var mIntent= Intent(this,LetsMeetActivity::class.java)
+            tvStepSix.visibility = View.GONE
+            var mIntent = Intent(this, LetsMeetActivity::class.java)
             startActivity(mIntent)
 
         }
         id_add_time.setOnClickListener {
-            setTimerAgain(5)
+            when (addTime) {
+                1L -> {
+                    setTimerAgain(addTime)
+                    addTime = 5
+                    id_add_time.text = "+ 5 Min"
+                }
+                5L -> {
+                    setTimerAgain(addTime)
+                    addTime = 0
+                }
+                else -> Toast.makeText(this,"Now you could not add more time.",Toast.LENGTH_LONG).show()
+            }
         }
 
 
-
     }
 
 
-
-    fun goTonext(view: View)
-    {
-        setpThree.visibility=View.VISIBLE
-        setpTwo.visibility=View.GONE
+    fun goTonext(view: View) {
+        setpThree.visibility = View.VISIBLE
+        setpTwo.visibility = View.GONE
     }
 
-    fun setProgressStart()
-    {
+    fun setProgressStart() {
         progressBar.visibility = View.VISIBLE
         two_progrss_txt.visibility = View.VISIBLE
         setupProgressTimer()
+
+    }
+
+    private fun setProgressMatch() {
+
 
     }
 
@@ -111,7 +122,7 @@ class MatchQuestionnaireActivity : AppCompatActivity() {
 
 
         Thread(Runnable {
-            while (pStatus >= 0 ) {
+            while (pStatus >= 0) {
                 pStatus -= 1
                 pStatusVisible -= 1
 
@@ -122,8 +133,8 @@ class MatchQuestionnaireActivity : AppCompatActivity() {
                     if (pStatusVisible == 0) {
                         runOnUiThread {
                             runOnUiThread {
-                                setpThree.visibility=View.VISIBLE
-                                setpThree.visibility=View.GONE
+                                setpThree.visibility = View.VISIBLE
+                                setpThree.visibility = View.GONE
                             }
                         }
                     }
@@ -142,23 +153,17 @@ class MatchQuestionnaireActivity : AppCompatActivity() {
     }
 
 
-    private fun setupTimerProgress(){
+    private fun startTimer(noOfMinutes: Long) {
 
-    }
-
-
-
-
-    private fun startTimer(noOfMinutes:Long) {
-
-         countDownTimer =( object: CountDownTimer(noOfMinutes,1000){
+        countDownTimer = (object : CountDownTimer(noOfMinutes, 1000) {
             override fun onFinish() {
 
             }
 
             override fun onTick(millisUntilFinished: Long) {
                 //Convert milliseconds into hour,minute and seconds
-                currentTime=millisUntilFinished
+                currentTime = millisUntilFinished
+                pbMatchTime.progress = currentTime.toInt()
                 val hms = String.format(
                     "%02d:%02d:%02d",
                     TimeUnit.MILLISECONDS.toHours(
@@ -175,32 +180,32 @@ class MatchQuestionnaireActivity : AppCompatActivity() {
                         )
                     )
                 )
-                tvRemaningTime.setText(hms)//set text
+                tvRemaningTime.text = hms//set text
             }
 
         }).start()
 
 
-
     }
 
-    fun setTimer(min:Long)
-    {
-
-        startTimer(min);
+    private fun setTimer(min: Long) {
+        pbMatchTime.visibility = View.VISIBLE
+        pStatus = 15
+        val drawable = ContextCompat.getDrawable(this,R.drawable.circular_progress_bg)
+        pbMatchTime.progressDrawable = drawable
+        pbMatchTime.progress = 0
+        pbMatchTime.max = min.toInt()
+        startTimer(min)
     }
 
-    fun setTimerAgain(min:Long)
-    {
+    fun setTimerAgain(min: Long) {
         countDownTimer.cancel()
-        var nowTime=currentTime
-        nowTime=min*60*1000+currentTime
+        var nowTime = currentTime
+        nowTime = min * 60 * 1000 + currentTime
         setTimer(nowTime)
 
 
     }
-
-
 
 
 }
