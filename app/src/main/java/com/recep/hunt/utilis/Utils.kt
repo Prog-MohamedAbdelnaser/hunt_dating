14bed914207f4ed47850d0d7e01d0af8ca049022
mavyasoni <onlysoni3@gmail.com>
2019-12-12 02:27:37 +0530

    [fixed] search location user crash solved

--- app/src/main/java/com/recep/hunt/utilis/Utils.kt
+++ app/src/main/java/com/recep/hunt/utilis/Utils.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import com.recep.hunt.login.WelcomeScreenActivity
+import org.json.JSONException
 import org.json.JSONObject
 
 
@@ -19,15 +20,20 @@ object Utils {
         errorJsonString?.let { it1 ->
             if (it1.isNotEmpty()) {
                 context?.let {
-                    val mJsonObject = JSONObject(errorJsonString)
-                    val status = mJsonObject.optString("status")
-                    val message = mJsonObject.optString("message")
-                    if (status == "9") {
-                        val mIntent = Intent("ACTION_SESSION_EXPIRE")
-                        mIntent.putExtra("INTENT_MESSAGE",message)
-                        context.sendBroadcast(mIntent)
-                        return true
+                    try{
+                        val mJsonObject = JSONObject(errorJsonString)
+                        val status = mJsonObject.optString("status")
+                        val message = mJsonObject.optString("message")
+                        if (status == "9") {
+                            val mIntent = Intent("ACTION_SESSION_EXPIRE")
+                            mIntent.putExtra("INTENT_MESSAGE",message)
+                            context.sendBroadcast(mIntent)
+                            return true
+                        }
+                    }catch (e: JSONException){
+                        e.printStackTrace()
                     }
+
                 }
             }
         }
