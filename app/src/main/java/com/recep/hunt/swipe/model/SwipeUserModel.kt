db6eb3509ac89e0a90cc9d5ed958bdfbfaae1c49
Irfan ullah <itsme.irfanullah@gmail.com>
2020-01-06 23:14:35 +0500

    SwipeUserModel Issue

--- app/src/main/java/com/recep/hunt/swipe/model/SwipeUserModel.kt
+++ app/src/main/java/com/recep/hunt/swipe/model/SwipeUserModel.kt
@@ -7,7 +7,6 @@ import com.recep.hunt.model.usersList.BasicInfo
 import com.recep.hunt.model.usersList.Data
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class SwipeUserModel(val id:Int,
                           val locationName:String,
                           val firstName:String,
@@ -22,4 +21,53 @@ data class SwipeUserModel(val id:Int,
                           val for_friendship:String,
                           val images:ArrayList<String>?,
                           val basicInfo: BasicInfo?
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readFloat(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readArrayList(ClassLoader::class.java.classLoader) as ArrayList<String>,
+        parcel.readParcelable(BasicInfo::class.java.classLoader)
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(locationName)
+        parcel.writeString(firstName)
+        parcel.writeInt(age)
+        parcel.writeString(title)
+        parcel.writeString(detail)
+        parcel.writeFloat(totalMatching)
+        parcel.writeInt(totalMeeting)
+        parcel.writeString(is_online)
+        parcel.writeString(for_date)
+        parcel.writeString(for_bussiness)
+        parcel.writeString(for_friendship)
+        parcel.writeList(images)
+        parcel.writeParcelable(basicInfo, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<SwipeUserModel> {
+        override fun createFromParcel(parcel: Parcel): SwipeUserModel {
+            return SwipeUserModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SwipeUserModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
