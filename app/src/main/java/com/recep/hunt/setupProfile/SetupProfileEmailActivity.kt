68e11266f31a2d244e31f9166983cc3147caa79a
michaeljohndamo1 <michaeljohndamo1@gmail.com>
2020-01-12 01:19:03 +0800

    Added backthread validation #94

--- app/src/main/java/com/recep/hunt/setupProfile/SetupProfileEmailActivity.kt
+++ app/src/main/java/com/recep/hunt/setupProfile/SetupProfileEmailActivity.kt
@@ -1,8 +1,18 @@
 package com.recep.hunt.setupProfile
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.kaopiz.kprogresshud.KProgressHUD
 import com.orhanobut.logger.Logger
 import com.recep.hunt.R
 import com.recep.hunt.api.ApiClient
@@ -10,11 +20,17 @@ import com.recep.hunt.model.CheckUserEmail
 import com.recep.hunt.model.isEmailRegister.isEmailRegisterResponse
 import com.recep.hunt.utilis.*
 import kotlinx.android.synthetic.main.activity_email_profile_xml.*
+import org.jetbrains.anko.find
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SetupProfileEmailActivity : BaseActivity() {
+class SetupProfileEmailActivity : BaseActivity(), View.OnClickListener {
+
+
+    val emailCheckStatus: MutableLiveData<Boolean> = MutableLiveData()
+    private lateinit var dialog: KProgressHUD
+    private var emailAddress: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,39 +39,36 @@ class SetupProfileEmailActivity : BaseActivity() {
         getBackButton().setOnClickListener { finish() }
         getBaseCancelBtn().setOnClickListener { Helpers.segueToSocialLoginScreen(this) }
 
+        dialog = Helpers.showDialog(this, this, "Processing")
+
+
+
+        email_address_next_btn.setOnClickListener(this)
+
+        setObservers()
         init()
     }
 
 
     private fun init() {
-        email_address_next_btn.setOnClickListener {
-            val emailAddress = email_address_editText.text.toString()
 
-            when {
-                emailAddress.isEmpty() -> {
-                    Helpers.showErrorSnackBar(
-                        this@SetupProfileEmailActivity,
-                        resources.getString(R.string.complete_form),
-                        resources.getString(R.string.complete_form)
-                    )
-                }
-                !isEmailValid(emailAddress) -> {
-                    Helpers.showErrorSnackBar(
-                        this@SetupProfileEmailActivity,
-                        resources.getString(R.string.complete_form),
-                        resources.getString(R.string.invalid_email_address)
-                    )
-                }
 
-                !checkIsUserRegister(emailAddress) -> {
+    }
+
+    private fun setObservers() {
+
+        emailCheckStatus.observe(this, Observer {
+            //process
+            Logger.d("Observer: $it")
+            when (it) {
+                false -> {
                     Helpers.showErrorSnackBar(
                         this@SetupProfileEmailActivity,
                         resources.getString(R.string.complete_form),
                         resources.getString(R.string.already_registered_email)
                     )
                 }
-
-                else -> {
+                true -> {
                     SharedPrefrenceManager.setUserEmail(
                         this@SetupProfileEmailActivity,
                         emailAddress
@@ -63,17 +76,54 @@ class SetupProfileEmailActivity : BaseActivity() {
                     launchActivity<SetupProfileDobActivity>()
                 }
             }
-        }
-
-
+        })
     }
+
+    override fun onClick(p0: View?) {
+        if (p0 != null) {
+            when (p0.id) {
+                R.id.email_address_next_btn -> {
+                    dialog.show()
+
+                    emailAddress = email_address_editText.text.toString()
+                    when {
+
+
+                        emailAddress.isEmpty() -> {
+
+                            Helpers.showErrorSnackBar(
+                                this@SetupProfileEmailActivity,
+                                resources.getString(R.string.complete_form),
+                                resources.getString(R.string.complete_form)
+                            )
+                        }
+                        !isEmailValid(emailAddress) -> {
+
+                            Helpers.showErrorSnackBar(
+                                this@SetupProfileEmailActivity,
+                                resources.getString(R.string.complete_form),
+                                resources.getString(R.string.invalid_email_address)
+                            )
+                        }
+
+                        else -> {
+
+                            checkIsUserRegister(emailAddress)
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
 
     private fun isEmailValid(email: CharSequence): Boolean {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
-    fun checkIsUserRegister(email: String): Boolean {
-        var isEmailAlreadyUse = false
+    private fun checkIsUserRegister(email: String) {
         val checkIsEmailValid = CheckUserEmail(email)
 
         val call = ApiClient.getClient.checkIsEmailRegister(
@@ -91,22 +141,15 @@ class SetupProfileEmailActivity : BaseActivity() {
                 response: Response<isEmailRegisterResponse>
             ) {
                 response.body()?.let {
-                    if (!it.status) {
-                        Toast.makeText(
-                            this@SetupProfileEmailActivity,
-                            it.message,
-                            Toast.LENGTH_SHORT
-                        )
-                            .show()
-                    }
-                    isEmailAlreadyUse = it.status
+                    emailCheckStatus.postValue(it.status)
 
                 }
             }
 
-
         })
 
-        return isEmailAlreadyUse
+
     }
+
+
 }
