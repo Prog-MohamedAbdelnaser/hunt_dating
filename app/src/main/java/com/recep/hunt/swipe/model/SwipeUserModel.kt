20a142275c3682fc441a096da2a05f87eb0e1760
Christian Callelero <crcallelero@gmail.com>
2020-01-12 00:43:11 +0800

    fixed missing creator field #82

--- app/src/main/java/com/recep/hunt/swipe/model/SwipeUserModel.kt
+++ app/src/main/java/com/recep/hunt/swipe/model/SwipeUserModel.kt
@@ -61,15 +61,6 @@ data class SwipeUserModel(val id:Int,
         return 0
     }
 
-//    companion object CREATOR : Parcelable.Creator<SwipeUserModel> {
-//        override fun createFromParcel(parcel: Parcel): SwipeUserModel {
-//            return SwipeUserModel(parcel)
-//        }
-//
-//        override fun newArray(size: Int): Array<SwipeUserModel?> {
-//            return arrayOfNulls(size)
-//        }
-//    }
 
     companion object{
         @JvmField
